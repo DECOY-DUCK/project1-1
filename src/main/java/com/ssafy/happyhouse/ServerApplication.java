c132f4e6e8aa96399d44f63e8dff640f0f7c3afc
web-doh <cocov2222@gmail.com>
2021-11-18 13:46:36 +0900

    modify initial settings

--- src/main/java/com/ssafy/happyhouse/ServerApplication.java
+++ src/main/java/com/ssafy/happyhouse/ServerApplication.java
@@ -2,8 +2,16 @@ package com.ssafy.happyhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+
+import com.ssafy.happyhouse.config.AdminKeyConfig;
+import com.ssafy.happyhouse.config.AuthKeyConfig;
+import com.ssafy.happyhouse.config.CorsConfig;
 
 @SpringBootApplication
+@PropertySource("classpath:/application.properties")
+@EnableConfigurationProperties(value = {AuthKeyConfig.class, AdminKeyConfig.class, CorsConfig.class})
 public class ServerApplication {
 
 	public static void main(String[] args) {
